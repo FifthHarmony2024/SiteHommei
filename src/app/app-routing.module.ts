71a9e0e47eb4d863873ad10d589be7588ddd0c0a
fifthharmonyfor2024 <fifthharmonyfor2024@gmail.com>
2024-08-12 10:58:16 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpresaComponent } from './Pages/empresa/empresa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: '/empresa', pathMatch: 'full'},
+  {path: 'empresa', component: EmpresaComponent},
+  { path: '**', redirectTo: '/empresa', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmpresaComponent } from './Pages/empresa/empresa.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpresaComponent,
+    HeaderComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
