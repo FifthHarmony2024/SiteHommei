30389173c85a1422e8267b44517f158c10f1bcfc
eloah <eloah@example.com>
2024-08-15 15:22:26 -0300

    a

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmpresaComponent } from './Pages/empresa/empresa.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: '/empresa', pathMatch: 'full'},
+  {path: 'empresa', component: EmpresaComponent},
+  { path: '**', redirectTo: '/empresa', pathMatch: 'full' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmpresaComponent } from './Pages/empresa/empresa.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpresaComponent,
+    HeaderComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
